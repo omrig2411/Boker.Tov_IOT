bc14432cacaedce81b8f29b4b0ae257359622179
og <omrig2411@gmail.com>
2021-03-23 11:18:05 +0200

    commit on 'main'

---
+++ esp32_bed/src/main.cpp
@@ -0,0 +1,197 @@
+// Import required libraries
+#include <Arduino.h>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <analogWrite.h>
+#include <esp_now.h>
+
+//wifi credentials
+#define WIFI_NETWORK "AndroidO"
+#define WIFI_PASSWORD "123456789"
+#define WIFI_TIMEOUT_MS 10000
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0xC5, 0x49, 0x38};
+
+const int WIFIpin = 25;
+
+//fsr variables
+const int fsrAnalogPin1 = 34; // first FSR is connected to analog 34
+const int fsrAnalogPin2 = 35; // second FSR is connected to analog 35
+int fsrReading1;              // the analog reading from the FSR resistor divider
+int fsrReading2;
+int LEDbrightness1;
+int LEDbrightness2;
+const int FSRLEDpin1 = 32;
+const int FSRLEDpin2 = 33;
+
+// data to to be sent to esp32 box board
+int outgoingFSR1;      // the analog reading from the FSR resistor divider
+int outgoingFSR2;
+
+// incoming data from esp32 box board
+boolean startDataCollection;
+
+typedef struct struct_message {
+  int FSR1;
+  int FSR2;
+  boolean startSensors = 0;
+} struct_message;
+
+// Create a struct_message called incomingStartCommand to hold 
+struct_message incomingStartCommand;
+
+// Create a struct_message to hold outgoing sensor readings
+struct_message outgoingReadings;
+
+// Variable to store if sending data was successful
+String success; 
+
+// Register peer variable
+esp_now_peer_info_t peerInfo;
+
+//wifi connection
+void connectToWiFi()
+{
+  Serial.print("connecting to wifi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+  unsigned long startAttemptTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
+  {
+    Serial.print(".");
+    delay(1000);
+  };
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Failed!");
+    Serial.println("trying again...");
+    // WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+    connectToWiFi();
+  }
+  else
+  {
+    Serial.print("Connected! IP:");
+    Serial.println(WiFi.localIP());
+    digitalWrite(WIFIpin, HIGH);
+  }
+}
+
+//callback upon sending data
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println("Last Packet Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Fail" : "Delivery Success");
+  Serial.println("--------------------");
+  if (status ==1) {
+    success = "Delivery Success :)";
+  }else {
+  success = "Delivery Fail :(";
+  }
+}
+
+//callback upon recieving data
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len){
+  memcpy(&incomingStartCommand, incomingData, sizeof(incomingStartCommand));
+  Serial.print("Bytes recieved: ");
+  Serial.println(len);
+  startDataCollection = incomingStartCommand.startSensors;
+}
+
+//sensor reading to be sent
+void getReadings() {
+  if (startDataCollection == 1){
+    fsrReading1 = analogRead(fsrAnalogPin1);
+    fsrReading2 = analogRead(fsrAnalogPin2);
+  }
+}
+
+void updateDisplay() {
+  //Serial monitor display
+  Serial.print("sensors start command: ");
+  Serial.println(startDataCollection);
+  Serial.println("--------------------");
+}
+
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  connectToWiFi();
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  // esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+
+  pinMode(FSRLEDpin1, OUTPUT);
+  pinMode(FSRLEDpin2, OUTPUT);
+  pinMode(WIFIpin, OUTPUT);
+}
+
+void loop()
+{
+  getReadings();
+
+  // Set values to send
+  outgoingReadings.FSR1 = fsrReading1;
+  outgoingReadings.FSR2 = fsrReading2;
+
+  Serial.print("analog reading 1 = ");
+  Serial.println(fsrReading1);
+  Serial.print("analog reading 2 = ");
+  Serial.println(fsrReading2);
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &outgoingReadings, sizeof(outgoingReadings));
+
+  if(result == ESP_OK) {
+    Serial.println("Sent successfully");
+  } 
+  else {
+    Serial.println("Error sending the data");
+  }
+
+  // we'll need to change the range from the analog reading (0-1023) down to the range
+  // used by analogWrite (0-255) with map!
+  LEDbrightness1 = map(fsrReading1, 0, 1023, 0, 255);
+  LEDbrightness2 = map(fsrReading2, 0, 1023, 0, 255);
+
+  // LED gets brighter the harder you press
+  analogWrite(FSRLEDpin1, LEDbrightness1);
+  analogWrite(FSRLEDpin2, LEDbrightness2);
+
+  Serial.println("--------------------");
+
+  updateDisplay();
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("wifi Disconnected! ");
+    digitalWrite(WIFIpin, LOW);
+    connectToWiFi();
+  }
+  delay(1000);
+}
---
+++ esp32_box/src/main.cpp
@@ -0,0 +1,287 @@
+// Import required libraries
+#include <Arduino.h>
+#include <analogWrite.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <esp_now.h>
+#include "WiFi.h"
+#include "Wire.h"
+#include "LiquidCrystal_I2C.h"
+
+
+#define TIMESECONDS 3
+#define WIFI_NETWORK "AndroidO"
+#define WIFI_PASSWORD "123456789"
+#define WIFI_TIMEOUT_MS 10000
+
+// esp_now MAC adress of bed board
+uint8_t broadcastAddress[] = {0x30, 0xAE, 0xA4, 0x99, 0xC6, 0x00};
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// ntp client variables
+WiFiUDP ntpUDP;
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "il.pool.ntp.org", 2*3600, 6000000);
+const int WIFIpin =  25;
+
+// PIR module and LED variables
+const int PIRpin =  26;      // the number of the LED pin
+const int PIR = 27;
+int ledState = LOW;             // ledState used to set the LED
+        
+// timer variables
+unsigned long now_M = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+
+// FSR variables
+int LEDbrightness1;
+int LEDbrightness2;
+const int FSRLEDpin1 = 32;
+const int FSRLEDpin2 = 33;
+
+// incoming data from esp32 bed board
+int incomingFSR1;      
+int incomingFSR2;
+
+// button variables
+const int buttonPin = 17;     // the number of the pushbutton pin
+const int ledPin =  18;      // the number of the LED pin
+int buttonState = 0;         // variable for reading the pushbutton status
+
+// data to to be sent to esp32 bed board
+boolean startDataCollection;
+
+typedef struct struct_message {
+  int FSR1;
+  int FSR2;
+  boolean startSensors;
+} struct_message;
+
+// Create a struct_message called outgoingStartCommand to hold 
+struct_message outgoingStartCommand;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+// Variable to store if sending data was successful
+String success; 
+
+// Register peer variable
+esp_now_peer_info_t peerInfo;
+
+// create an LCD object (Hex address, # characters, # rows)
+// my LCD display in on Hex address 27 and is a 20x4 version
+LiquidCrystal_I2C lcd(0x27, 20, 4); 
+
+//wifi connection
+void connectToWiFi(){
+  Serial.print("Connect wifi");
+  lcd.clear();
+  lcd.print("Connect wifi");
+
+  WiFi.mode(WIFI_AP_STA);  
+  WiFi.softAP("esp_daddy", "123456789");
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+  unsigned long startAttemptTime = millis();
+  
+  lcd.setCursor(0,1);
+  while(WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS){
+    Serial.print(".");
+    lcd.print(".");
+    delay(1000);
+  };
+
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Failed!");
+    lcd.clear();
+    lcd.print("Failed!");
+    Serial.println("Trying again...");
+    lcd.setCursor(1,1);
+    lcd.print("Trying again...");
+    // WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+    connectToWiFi();
+
+  }else{
+    Serial.print("Connected! IP:");
+    lcd.clear();
+    lcd.print("Connected! IP:");
+    lcd.setCursor(0,1);
+    Serial.println(WiFi.localIP());
+    lcd.print(WiFi.localIP());
+    digitalWrite(WIFIpin, HIGH);
+    delay(3000);
+  }
+}
+
+void currentTime(){
+    lcd.clear();
+    lcd.print(timeClient.getFormattedTime());
+}
+
+//callback upon sending data
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.println("Last Packet Send Status:");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Fail" : "Delivery Success");
+  Serial.println("--------------------");
+  if (status ==1) {
+    success = "Delivery Success :)";
+  }else {
+  success = "Delivery Fail :(";
+  }
+}
+
+//callback upon recieving data
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len){
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes recieved: ");
+  Serial.println(len);
+  incomingFSR1 = incomingReadings.FSR1;
+  incomingFSR2 = incomingReadings.FSR2;
+}
+
+//used to trigger data collection in the bed board
+void getReadings() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  
+  startDataCollection = buttonState;
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+void updateDisplay() {
+  lcd.clear();
+  lcd.print("bed movements:");
+  lcd.setCursor(0, 1);
+  lcd.print("1: ");
+  lcd.print(incomingFSR1 + " ");
+  lcd.print("2: ");
+  lcd.println(incomingFSR2 + " ");
+
+  //Serial monitor display
+  Serial.println("bed movements:");
+  Serial.print("FSR1: ");
+  Serial.println(incomingFSR1);
+  Serial.print("FSR2: ");
+  Serial.println(incomingFSR2);
+  Serial.println("--------------------");
+}
+
+
+// Checks if motion was detected, interupt sets LED HIGH and starts a timer
+void IRAM_ATTR detectsMovement() {
+  Serial.println("MOTION DETECTED AT:");
+
+  digitalWrite(PIRpin, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+  
+  Serial.println(daysOfTheWeek[timeClient.getDay()]);
+  Serial.println(timeClient.getFormattedTime());
+  Serial.println("--------------------");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  connectToWiFi();
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  // esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+
+  //begin tnpclient
+  timeClient.begin();
+  // set the digital pin as output:
+  pinMode(PIRpin, OUTPUT);
+  pinMode(WIFIpin, OUTPUT);
+  digitalWrite(PIRpin, LOW);
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
+  // PIR Motion Sensor mode INPUT_PULLUP
+  pinMode(PIR, INPUT_PULLUP);
+
+  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+  attachInterrupt(digitalPinToInterrupt(PIR), detectsMovement, HIGH);
+}
+
+void loop() {
+  timeClient.update();
+  now_M = millis();
+
+  currentTime();
+
+  getReadings();
+
+  // Set values to send
+  outgoingStartCommand.startSensors = startDataCollection;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &outgoingStartCommand, sizeof(outgoingStartCommand));
+
+  if(result == ESP_OK) {
+    Serial.println("Sent successfully");
+  } 
+  else {
+    Serial.println("Error sending the data");
+  }
+
+  if (startTimer && (now_M - lastTrigger > (TIMESECONDS * 1000))) {
+    Serial.println("Motion stopped");
+    Serial.println("--------------------");
+    
+    digitalWrite(PIRpin, LOW);
+    startTimer = false;
+  }
+
+  updateDisplay();
+
+
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("wifi Disconnected! ");
+    digitalWrite(WIFIpin, LOW);
+    connectToWiFi();
+    
+  }
+  delay(1000);
+}
